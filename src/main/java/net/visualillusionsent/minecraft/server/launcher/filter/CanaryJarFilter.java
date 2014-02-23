5b8a4a2169705f21a3519bced7392a221a9e15b0
darkdiplomat <darkdiplomat@visualillusionsent.net>
2014-02-23 15:09:48 -0600

    minor changes

--- src/main/java/net/visualillusionsent/minecraft/server/launcher/filter/CanaryJarFilter.java
+++ src/main/java/net/visualillusionsent/minecraft/server/launcher/filter/CanaryJarFilter.java
@@ -1,18 +1,18 @@
 /*
- * This file is part of VisualIllusionsMinecraftServerLauncher.
+ * This file is part of Visual Illusions Minecraft Server Launcher.
  *
  * Copyright © 2013-2014 Visual Illusions Entertainment
  *
- * VisualIllusionsMinecraftServerLauncher is free software: you can redistribute it and/or modify
+ * Visual Illusions Minecraft Server Launcher is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
  *
- * VisualIllusionsMinecraftServerLauncher is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with VisualIllusionsMinecraftServerLauncher.
+ * You should have received a copy of the GNU General Public License along with this program.
  * If not, see http://www.gnu.org/licenses/gpl.html.
  *
  * Minecraft is the property of Mojang AB/Notch Development AB
